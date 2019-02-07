311f2ea7c8773ce071abbae856991d33ccdeea6d
Andrew Betts <andrew@trib.tv>
2019-02-07 19:31:05 +0000

    Simplify demo error capturing

--- public/demo-utils.js
+++ public/demo-utils.js
@@ -1,13 +1,24 @@
 
-window.DemoUtils = {
-  reportDemoResult: (isWorking, { failReason, resultDetail } = {}) => {
-    const thisURL = new URL(window.location.href);
-    window.top.postMessage({
-      type: "feature-test-result",
-      demoID: thisURL.searchParams.get('demoID'),
-      isWorking: isWorking,
-      failReason: failReason,
-      resultDetail: resultDetail
-    }, "*");
-  }
-};
+window.DemoUtils = (() => {
+  let timeout;
+  return {
+    setupTimeout: (msg, dur) => {
+      timeout = setTimeout(() => {
+        window.DemoUtils.reportDemoResult(false, {resultDetail: msg});
+      }, dur || 500);
+    },
+    reportDemoResult: (isWorking, { failReason, resultDetail } = {}) => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+      const thisURL = new URL(window.location.href);
+      window.top.postMessage({
+        type: "feature-test-result",
+        demoID: thisURL.searchParams.get('demoID'),
+        isWorking: isWorking,
+        failReason: failReason,
+        resultDetail: resultDetail
+      }, "*");
+    }
+  };
+})();
